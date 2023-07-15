27ddec4837e0456c065e4d98ab5153c8495175ed
TTHPATEL <thchhbhaiya@gmail.com>
2023-07-16 00:47:28 +0530

    footer,header,nav

---
+++ extra/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+	   <p>&copy; 2023 Glance Design Dashboard. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">DSS TEAM</a></p>		
+	</div>
+
+	<!-- Hello -->
---
+++ extra/leftnavigationbar.php
@@ -0,0 +1,161 @@
+<aside class="sidebar-left">
+      <nav class="navbar navbar-inverse">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+            <h1><a class="navbar-brand" href="#"><span class="fa fa-area-chart"></span>DSS<span class="dashboard_text">Design dashboard</span></a></h1>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="sidebar-menu">
+              <li class="header">MAIN NAVIGATION</li>
+              <li class="treeview">
+                <a href="dashboard.php">
+                <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                </a>
+              </li>
+                <li class="treeview">
+                <a href="#">
+                <i class="fa fa-building"></i>
+                <span>Wing</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                     <ul class="treeview-menu">
+                         <li><a href="add_wing.php"><i class="fa fa-angle-right"></i>Add Wing</a></li>
+                         <li><a href="list_wing.php"><i class="fa fa-angle-right"></i>Wing List</a></li>
+                    </ul>
+
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-user"></i>
+                <span>Staff</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                     <ul class="treeview-menu">
+                         <li><a href="add_wk_staff.php"><i class="fa fa-angle-right"></i>Add Staff</a></li>
+                         <li><a href="list_wk_staff.php"><i class="fa fa-angle-right"></i>Staff List</a></li>
+                         <li><a href="add_staff_sal.php"><i class="fa fa-angle-right"></i>Add Staff Salary</a></li>
+                         <li><a href="list_staff_sal.php"><i class="fa fa-angle-right"></i>Staff Salary</a></li>
+                    </ul>
+
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-money"></i>
+                <span>Expenses & Incomes</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                     <ul class="treeview-menu">
+                      <li><a href="add_expense_type.php"><i class="fa fa-angle-right"></i>Add Expenses Type</a></li>
+                      <li><a href="list_exp_type.php"><i class="fa fa-angle-right"></i>Expenses Type List</a></li>
+                         <li><a href="add_expense.php"><i class="fa fa-angle-right"></i>Add Expenses</a></li>
+                         <li><a href="list_expense.php"><i class="fa fa-angle-right"></i>Expenses List</a></li>
+                         <li><a href="add_extra_income.php"><i class="fa fa-angle-right"></i>Add Incomes</a></li>
+                         <li><a href="list_ext_income.php"><i class="fa fa-angle-right"></i>Incomes List</a></li>
+                    </ul>
+
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-user"></i>
+                <span>User</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                     <ul class="treeview-menu">
+                         <li><a href="add_user_role.php"><i class="fa fa-angle-right"></i>Add User Role</a></li>
+                         <li><a href="list_user_role.php"><i class="fa fa-angle-right"></i>User Role List</a></li>
+                         <li><a href="add_user.php"><i class="fa fa-angle-right"></i>Add User</a></li>
+                         <li><a href="list_user.php"><i class="fa fa-angle-right"></i>User List</a></li>
+                    </ul>
+
+              </li>
+              
+ <li class="treeview">
+                <a href="#">
+                <i class="fa fa-building"></i>
+                <span>Maintenance</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                     <ul class="treeview-menu">
+                      <li><a href="add_maintenance_month.php"><i class="fa fa-angle-right"></i>Add Maintenance Month</a></li>
+                      <li><a href="list_maintenance_month.php"><i class="fa fa-angle-right"></i>Maintenance Month List</a></li>
+
+                      <li><a href="user_main_list.php"><i class="fa fa-angle-right"></i>User Maintenance  List</a></li>
+                    </ul>
+               </li>
+
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-calendar"></i>
+                <span>Event</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                     <ul class="treeview-menu">
+                      <li><a href="add_event.php"><i class="fa fa-angle-right"></i>Add Event</a></li>
+                      <li><a href="list_event.php"><i class="fa fa-angle-right"></i>Event List</a></li>
+                    </ul>
+               </li>     
+              
+               <li class="treeview">
+                <a href="#">
+                <i class="fa fa-bell"></i>
+                <span>Notification</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                     <ul class="treeview-menu">
+                      <li><a href="add_noti.php"><i class="fa fa-angle-right"></i>Add Notification</a></li>
+                      <li><a href="list_noti.php"><i class="fa fa-angle-right"></i>Notification List</a></li>
+                    </ul>
+               </li>  
+               <li class="treeview">
+                <a href="visitor_list.php">
+                <i class="fa fa-user"></i> <span>Visitor List</span>
+                </a>
+              </li>
+              <li class="treeview">
+                <a href="complaint_list.php">
+                <i class="fa fa-list"></i> <span>Complaint List</span>
+                </a>
+              </li>   
+                <li class="treeview">
+                <a href="logout.php">
+                <i class="fa fa-sign-out"></i> <span>Logout</span>
+                </a>
+              </li>
+<!-- 
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-laptop"></i>
+                <span>Category</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+
+                <ul class="treeview-menu">
+                  <li><a href="add_wing.php"><i class="fa fa-angle-right"></i>Add_wing</a></li>
+                  <li><a href="wk_staff.php"><i class="fa fa-angle-right"></i>Add_staff</a></li>
+                  <li><a href="add_event.php"><i class="fa fa-angle-right"></i>Add_event</a></li>
+                  <li><a href="add_expense.php"><i class="fa fa-angle-right"></i>Add_expense</a></li>
+                  <li><a href="add_expense_type.php"><i class="fa fa-angle-right"></i>Add_expense_type</a></li>
+                   <li><a href="extra_income.php"><i class="fa fa-angle-right"></i>Add_Extra_income</a></li>
+                   <li><a href="user_maint.php"><i class="fa fa-angle-right"></i>User maintence</a></li>
+                    <li><a href="add_noti.php"><i class="fa fa-angle-right"></i>Add_notification</a></li>
+                    <li><a href="add_staff_sal.php"><i class="fa fa-angle-right"></i>Add_Staff_Salary</a></li>
+                     <li><a href="add_staff_role.php"><i class="fa fa-angle-right"></i>Add_Staff_Role</a></li>
+
+                </ul>
+              </li> -->
+
+             <!--  <li class="header">LABELS</li>
+              <li><a href="#"><i class="fa fa-angle-right text-red"></i> <span>Important</span></a></li>
+              <li><a href="#"><i class="fa fa-angle-right text-yellow"></i> <span>Warning</span></a></li>
+              <li><a href="#"><i class="fa fa-angle-right text-aqua"></i> <span>Information</span></a></li>
+            </ul>
+          </div> -->
+          <!-- /.navbar-collapse -->
+      </nav>
+    </aside>
+  </div>
